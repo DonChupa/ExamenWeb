997bfedf4fe4e4002479b0c49bf9f73b440248c1
ElMismisimo <joi.saez@duocuc.cl>
2023-07-07 21:39:37 -0400

    xd

--- ExamenconTomi/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def mostrar_index(request):
-    return render(request,'index.html')
--- m_user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import Model, IntegerField, ForeignKey, CASCADE, AutoField
-from django.contrib.auth.models import User
-from m_prod.models import Boleta
-# Create your models here.
-
-class DetalleBoleta(Model):
-    id =  AutoField(primary_key= True)
-    boleta = ForeignKey(Boleta, on_delete= CASCADE)
